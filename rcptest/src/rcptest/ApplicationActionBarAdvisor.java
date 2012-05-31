92381471d98fe858671a8f33cda7b518ae4da452
zhuchx <zhuchx1008@gmail.com>
2012-05-31 14:33:49 +0800

    增加工具栏

--- rcptest/src/rcptest/ApplicationActionBarAdvisor.java
+++ rcptest/src/rcptest/ApplicationActionBarAdvisor.java
@@ -1,8 +1,13 @@
 package rcptest;
 
+import org.eclipse.jface.action.ICoolBarManager;
 import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.action.ToolBarContributionItem;
+import org.eclipse.jface.action.ToolBarManager;
+import org.eclipse.swt.SWT;
 import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.actions.ActionFactory;
@@ -41,4 +46,12 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 		helpMenu.add(iAboutAction);
 	}
 
+	protected void fillCoolBar(ICoolBarManager coolBar) {
+		IToolBarManager toolbar = new ToolBarManager(SWT.FLAT | SWT.RIGHT);
+		coolBar.add(new ToolBarContributionItem(toolbar, "main"));
+		toolbar.add(iNewWindowAction);
+		toolbar.add(iSaveAction);
+		toolbar.add(iAboutAction);
+		toolbar.add(iExitAction);
+	}
 }
